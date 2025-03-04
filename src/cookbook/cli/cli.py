3aa108cfaece6b0316bc1049619417fd7b7ed3ce
Matt Jordan <mattj@allenai.org>
2025-03-04 14:55:33 -0800

    atually merged

--- src/cookbook/cli/cli.py
+++ src/cookbook/cli/cli.py
@@ -7,19 +7,14 @@ import click
 import yaml
 from beaker import Beaker
 from beaker.services.job import JobClient
-from olmo_core.utils import generate_uuid, prepare_cli_environment
 from tqdm import tqdm
 from yaspin import yaspin
 
 from cookbook.aliases import ExperimentConfig, LaunchGroup, validate_sources
 from cookbook.cli.eval import convert, evaluate
-from cookbook.utils.config import (
-    build_train_config,
-    config_from_path,
-    mk_experiment_group,
-    mk_launch_configs,
-)
+from cookbook.utils.config import build_train_config, config_from_path, mk_experiment_group, mk_launch_configs
 from cookbook.utils.data import get_token_counts_and_ratios
+from olmo_core.utils import generate_uuid, prepare_cli_environment
 
 logger = logging.getLogger(__name__)
 
@@ -63,14 +58,6 @@ def launch(config: Path, dry_run: bool, no_cache: bool, group_id: Optional[str] 
         experiment_config.dataset.sources, experiment_config.dataset.dtype, not no_cache
     )
 
-<<<<<<< HEAD
-    token_universe = ({}, 0)
-
-    logger.info("Token distribution by source:")
-    logger.info(token_universe)
-
-=======
->>>>>>> main
     if group_id:
         group_uuid = group_id
     else:
