b62c5ef5d1cd08d256c02368b5dcf41300e1cf95
Luca Soldaini <lucas@allenai.org>
2025-03-25 17:39:49 -0700

    style

--- src/cookbook/cli/core.py
+++ src/cookbook/cli/core.py
@@ -23,14 +23,12 @@ from cookbook.cli.utils import (
     make_aws_credentials,
 )
 
-
 logger = logging.getLogger(__name__)
 
 
 OLMO_CORE_TRAIN_COMMIT_HASH = "2f66fd95c17c9779be9930f8fb80803293c2dc30"
 NAMED_MIXES_CONFIG_DIR = Path(__file__).parent.parent / "data/mixes"
 OLMO_CORE_EXAMPLES_BASE_DIR = "src/scripts/train"
-
 
 
 def guess_model_size(model: str) -> int:
@@ -55,14 +53,14 @@ def estimate_batch_size(
     We estimate instant critical batch size as bs = factor * sqrt(total_steps)
     """
     if total_steps:
-        critical_batch_size = _factor * total_steps ** (1/2)
+        critical_batch_size = _factor * total_steps ** (1 / 2)
     elif total_tokens:
-        critical_batch_size = ((_factor ** 2) * (total_tokens / sequence_length)) ** (1/3)
+        critical_batch_size = ((_factor**2) * (total_tokens / sequence_length)) ** (1 / 3)
     else:
         raise ValueError("Either total_steps or total_tokens must be provided")
 
     safe_batch_size = int(2 ** math.floor(math.log2(critical_batch_size)))
-    max_batch_size = 2 ** 24 // sequence_length # 16M tokens from llama 3 405B
+    max_batch_size = 2**24 // sequence_length  # 16M tokens from llama 3 405B
     return min(safe_batch_size, max_batch_size)
 
 
@@ -73,17 +71,10 @@ class ConfigLengths:
     global_batch_size: int
     warmup_steps: int
 
-
     @classmethod
     def parse(cls, model_script: str, env: PythonEnv, olmo_core_dir: str) -> "ConfigLengths":
         # try running in dry_run mode
-        command = [
-            env.python,
-            model_script,
-            "dry_run",
-            "temporary_run_name_name",
-            "temporary_cluster_name"
-        ]
+        command = [env.python, model_script, "dry_run", "temporary_run_name_name", "temporary_cluster_name"]
         process = subprocess.Popen(
             shlex.split(" ".join(command)),
             cwd=olmo_core_dir,
@@ -120,40 +111,44 @@ class ConfigLengths:
             sequence_length=sequence_length,
             rank_microbatch_size=rank_microbatch_size,
             global_batch_size=global_batch_size,
-            warmup_steps=warmup_steps
+            warmup_steps=warmup_steps,
         )
 
 
 @click.option(
-    '-d', '--data-mix',
+    "-d",
+    "--data-mix",
     type=str,
     required=True,
     help=(
         "Name or path of the data mix to use to train. "
         "Can be either an absolute path to a file, or name of file in the data/mixes directory."
-    )
+    ),
 )
 @click.option(
-    '-m', '--model',
+    "-m",
+    "--model",
     type=str,
     required=True,
     help=(
         "Name or path of the model to train. "
         "Can be either an absolute path to a file, or name of file in the OLMo Core's src/scripts/train directory."
-    )
+    ),
 )
 @click.option(
-    '-n', '--duration',
+    "-n",
+    "--duration",
     type=str,
     required=True,
     help=(
         "Duration of the training in tokens (t), steps (s), or epochs (e). "
         "Can be specified as a number followed by a unit (e.g., '10000 steps'), "
         "or using scientific notation (e.g., '1e9t'). "
-    )
+    ),
 )
 @click.option(
-    "-c", "--cluster",
+    "-c",
+    "--cluster",
     type=str,
     default="ai2/jupiter-cirrascale-2",
     help="Cluster(s) to use for training",
@@ -177,10 +172,7 @@ class ConfigLengths:
     default=OLMO_CORE_TRAIN_COMMIT_HASH,
 )
 @click.option(
-    "-g", "--num-gpus",
-    type=int,
-    default=8,
-    help="Number of GPUs to use for training. Each node has 8 GPUs."
+    "-g", "--num-gpus", type=int, default=8, help="Number of GPUs to use for training. Each node has 8 GPUs."
 )
 @click.option(
     "--dry-run",
@@ -201,20 +193,23 @@ class ConfigLengths:
     help="Enable or disable logging to comet. If logging to comet, must have COMET_API_KEY set in environment.",
 )
 @click.option(
-    "-w", "--workspace",
+    "-w",
+    "--workspace",
     type=str,
     required=True,
     default="ai2/oe-data",
     help="Beaker workspace to use for training.",
 )
 @click.option(
-    "-p", "--priority",
+    "-p",
+    "--priority",
     type=str,
     default="high",
     help="Priority of the job.",
 )
 @click.option(
-    "-b", "--budget",
+    "-b",
+    "--budget",
     type=str,
     default="ai2/oe-data",
     help="Budget to use for the job.",
@@ -226,7 +221,8 @@ class ConfigLengths:
     default=None,
 )
 @click.option(
-    "-i", "--beaker-image",
+    "-i",
+    "--beaker-image",
     type=str,
     default=None,
     help="Beaker image to use for training.",
@@ -256,8 +252,9 @@ def launch(
     olmo_core_dir = install_olmo_core(commit_hash=olmo_core_commit_hash, env=env)
 
     config_names = {str(p.stem): p for p in (Path(olmo_core_dir) / OLMO_CORE_EXAMPLES_BASE_DIR).glob("*.py")}
-    assert model in config_names or (Path(olmo_core_dir) / model).exists(), \
-        f"Model {model} not found in {config_names}"
+    assert (
+        model in config_names or (Path(olmo_core_dir) / model).exists()
+    ), f"Model {model} not found in {config_names}"
     model_script_path = str(config_names.get(model, Path(model)).relative_to(olmo_core_dir))
 
     flags: list[str] = []
@@ -299,7 +296,7 @@ def launch(
         secret_name="BEAKER_TOKEN",
         secret_value=beaker_token_value,
         workspace=workspace,
-        env=env     # pyright: ignore
+        env=env,  # pyright: ignore
     )
     env_secrets.append({"name": "BEAKER_TOKEN", "secret": beaker_token_name})
 
@@ -310,7 +307,7 @@ def launch(
             secret_name="WANDB_API_KEY",
             secret_value=wandb_api_secret_value,
             workspace=workspace,
-            env=env     # pyright: ignore
+            env=env,  # pyright: ignore
         )
         env_secrets.append({"name": "WANDB_API_KEY", "secret": wandb_api_secret_name})
     flags.append(f"--trainer.callbacks.wandb.enabled={use_wandb}")
@@ -322,7 +319,7 @@ def launch(
             secret_name="COMET_API_KEY",
             secret_value=comet_api_secret_value,
             workspace=workspace,
-            env=env     # pyright: ignore
+            env=env,  # pyright: ignore
         )
         env_secrets.append({"name": "COMET_API_KEY", "secret": comet_api_secret_name})
     flags.append(f"--trainer.callbacks.comet.enabled={use_comet}")
@@ -335,30 +332,28 @@ def launch(
             secret_name="AWS_ACCESS_KEY_ID",
             secret_value=aws_access_key_id,
             workspace=workspace,
-            env=env     # pyright: ignore
+            env=env,  # pyright: ignore
         )
         aws_secret_access_key_name = add_secret_to_beaker_workspace(
             secret_name="AWS_SECRET_ACCESS_KEY",
             secret_value=aws_secret_access_key,
             workspace=workspace,
-            env=env     # pyright: ignore
+            env=env,  # pyright: ignore
         )
         aws_config_name = add_secret_to_beaker_workspace(
             secret_name="AWS_CONFIG",
             secret_value=make_aws_config(profile_name="S3"),
             workspace=workspace,
-            env=env     # pyright: ignore
+            env=env,  # pyright: ignore
         )
 
         aws_credentials_name = add_secret_to_beaker_workspace(
             secret_name="AWS_CREDENTIALS",
             secret_value=make_aws_credentials(
-                aws_access_key_id=aws_access_key_id,
-                aws_secret_access_key=aws_secret_access_key,
-                profile_name="S3"
+                aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, profile_name="S3"
             ),
             workspace=workspace,
-            env=env     # pyright: ignore
+            env=env,  # pyright: ignore
         )
 
         env_secrets.append({"name": "AWS_ACCESS_KEY_ID", "secret": aws_access_key_id_name})
@@ -381,12 +376,16 @@ def launch(
         total_steps=duration_value if duration_unit_parsed == "steps" else None,
     )
     new_global_batch_size = new_batch_size * config_lengths.sequence_length
-    new_rank_microbatch_size = new_global_batch_size // (config_lengths.global_batch_size // config_lengths.rank_microbatch_size)
+    new_rank_microbatch_size = new_global_batch_size // (
+        config_lengths.global_batch_size // config_lengths.rank_microbatch_size
+    )
     flags.append(f"--data_loader.global_batch_size={new_global_batch_size}")
     flags.append(f"--train_module.rank_microbatch_size={new_rank_microbatch_size}")
 
     # we warm up for 10% or the previous warmup steps, whichever is smaller
-    new_total_steps = duration_value if duration_unit_parsed == "steps" else (duration_value // new_global_batch_size)
+    new_total_steps = (
+        duration_value if duration_unit_parsed == "steps" else (duration_value // new_global_batch_size)
+    )
     new_warmup_steps = min(config_lengths.warmup_steps, int(new_total_steps * 0.1))
     flags.append(f"--train_module.scheduler.warmup_steps={new_warmup_steps}")
 
@@ -408,25 +407,16 @@ def launch(
         flags.append(f"--launch.beaker_image={beaker_image}")
 
     # command
-    command = " ".join(
-        [
-            env.python,
-            model_script_path,
-            command,
-            run_name,
-            cluster,
-            *flags
-        ]
-    )
+    command = " ".join([env.python, model_script_path, command, run_name, cluster, *flags])
     print(f"\n\nCommand:\n{command}\nFrom:\n{olmo_core_dir}\n\n")
 
     if dry_run:
         return
 
     return subprocess.run(
-        shlex.split(command),
-        cwd=olmo_core_dir,
-        env=env.path(), stdout=sys.stdout, stderr=sys.stderr)
+        shlex.split(command), cwd=olmo_core_dir, env=env.path(), stdout=sys.stdout, stderr=sys.stderr
+    )
+
 
 @click.group()
 def cli():
