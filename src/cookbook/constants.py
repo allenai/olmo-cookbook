54697a6e73c5a813b698096d40e4947e54cd2903
Tyler Murray <tylerm@allenai.org>
2025-04-11 12:50:29 -0700

    Pin convert to later version

--- src/cookbook/constants.py
+++ src/cookbook/constants.py
@@ -15,7 +15,7 @@ OLMOE_UNSHARD_SCRIPT = "scripts/unshard.py"
 OLMO2_COMMIT_HASH = "69362b95c66655191d513e9c1420d54aa8477d92"
 OLMO2_UNSHARD_SCRIPT = "scripts/unshard.py"
 
-OLMO_CORE_COMMIT_HASH = "9bad23d9a78e62101699a585a8fde3d69dba5616"
+OLMO_CORE_COMMIT_HASH = "c149b73b4b02675140af67e1e32bac9d9bf116f8"
 OLMO_CORE_UNSHARD_CONVERT_SCRIPT = "src/examples/huggingface/convert_checkpoint_to_hf.py"
 
 DEFAULT_OLMOE_TOKENIZER = "allenai/eleuther-ai-gpt-neox-20b-pii-special"
