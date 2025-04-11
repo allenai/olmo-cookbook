777d65fd13bd8fb5abead5c0004a4039c25b086a
MayeeChen <mayeefchen@gmail.com>
2025-04-11 14:16:52 -0700

    fix comma typo

--- src/cookbook/utils/config.py
+++ src/cookbook/utils/config.py
@@ -195,7 +195,7 @@ def mk_launch_configs(group: ExperimentGroup, beaker_user: str) -> list[BeakerLa
                 "git submodule update --init --recursive",
                 "pip install -e '.[all]'",
                 # Temporary until they release a fix for 2.7.0
-                "pip install torch==2.7.0 torchaudio torchvision --index-url https://download.pytorch.org/whl/test/cu128"
+                "pip install torch==2.7.0 torchaudio torchvision --index-url https://download.pytorch.org/whl/test/cu128",
                 "pip freeze",
                 # Move AWS credentials from env to relevant files
                 "mkdir -p ~/.aws",
