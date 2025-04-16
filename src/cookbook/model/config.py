6efa999e53c3dae7106c0e5a35d8d1a9bc1f230a
Tyler Murray <tylerm@allenai.org>
2025-04-16 12:06:11 -0700

    Manually build superbpe tokenizer for now

--- src/cookbook/model/config.py
+++ src/cookbook/model/config.py
@@ -16,7 +16,13 @@ from olmo_core.train import TrainerConfig
 class Tokenizers(Enum):
     dolma2 = TokenizerConfig.dolma2()
     gpt_neox = TokenizerConfig.gpt_neox_olmo_dolma_v1_5()
-    superbpe_experimental = TokenizerConfig.from_hf("allenai/superbpe-experimental_v0.1.0")
+    superbpe_experimental = TokenizerConfig(
+        vocab_size=180021,
+        identifier="superbpe-experimental_v0.1.0",
+        bos_token_id=180000,
+        eos_token_id=180000,
+        pad_token_id=180001,
+    )
 
 
 @dataclass
