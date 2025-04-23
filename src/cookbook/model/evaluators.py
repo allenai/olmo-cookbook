131b2b439e36840f0afaa46eecaeda0da120478d
Tyler Murray <tylerm@allenai.org>
2025-04-23 09:57:38 -0700

    rename

--- src/cookbook/model/evaluators.py
+++ src/cookbook/model/evaluators.py
@@ -1,12 +1,12 @@
 from enum import Enum
-from typing import List, Literal, Union
+from typing import List, Union
 
 from olmo_eval import list_tasks
 
 
-DownstreamEvaluator = Enum("DownstreamEvaluator", {name: name for name in Union[list_tasks()] + ["ALL"]})
+DownstreamEvaluator = Enum("DownstreamEvaluator", {name: name for name in Union[list_tasks()] + ["all"]})
 
 
 def get_all_tasks() -> List[str]:
     """Return all downstream evaluation task names except 'all'."""
-    return [task.value for task in DownstreamEvaluator if task.name != "ALL"]  # type: ignore
+    return [task.value for task in DownstreamEvaluator if task.name != "all"]  # type: ignore
