9f783a9f114fe8fb13ee1d7d7a81c67070280224
Tyler Murray <tylerm@allenai.org>
2025-04-23 09:10:21 -0700

    unique task names

--- src/cookbook/model/evaluators.py
+++ src/cookbook/model/evaluators.py
@@ -10,4 +10,6 @@ class DownstreamEvaluator(Enum):
 
     # Dynamically add tasks from olmo_eval.list_tasks()
     for task in set(list_tasks()):
-        locals()[task.upper()] = task
+        task_upper = task.upper()
+        if task_upper not in locals():
+            locals()[task_upper] = task
