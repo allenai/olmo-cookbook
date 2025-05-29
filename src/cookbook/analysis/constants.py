5b4c6ec419719aaa86c25eaf290abc30018f8149
davidheineman <dheineman3@gatech.edu>
2025-05-29 14:13:12 -0700

    fix cache dir

--- src/cookbook/analysis/constants.py
+++ src/cookbook/analysis/constants.py
@@ -6,7 +6,7 @@ from platformdirs import user_cache_dir
 
 def get_cache_path(dashboard) -> Path:
     cache_dir = user_cache_dir("cookbook")
-    if not os.path.exists(cache_dir):
-        os.makedirs(cache_dir, exist_ok=True)
     path = Path(cache_dir) / dashboard
+    if not os.path.exists(path):
+        os.makedirs(path, exist_ok=True)
     return path
