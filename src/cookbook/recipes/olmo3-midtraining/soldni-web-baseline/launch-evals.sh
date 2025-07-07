088ecdf26a86a2aa3c317283e930c722f2f8f553
Luca Soldaini <lucas@allenai.org>
2025-07-07 10:26:25 -0700

    move

---
+++ src/cookbook/recipes/olmo3-midtraining/soldni-web-baseline/launch-evals.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Moving checkpoints to weka
+
+uv run python -m cookbook.remote gs://ai2-llm/checkpoints/lucas/olmo3-microanneal-all-dressed-v18-ce7c0c2a/step4769 weka://oe-training-default/ai2-llm/checkpoints/lucas/olmo3-microanneal-all-dressed-v18-ce7c0c2a/step4769
+uv run python -m cookbook.remote gs://ai2-llm/checkpoints/lucas/olmo3-microanneal-all-dressed-v20-1e29004d/step4769  weka://oe-training-default/ai2-llm/checkpoints/lucas/olmo3-microanneal-all-dressed-v20-1e29004d/step4769
+uv run python -m cookbook.remote gs://ai2-llm/checkpoints/lucas/olmo3-microanneal-hq-web-baseline-0e513fe6/step4769  weka://oe-training-default/ai2-llm/checkpoints/lucas/olmo3-microanneal-hq-web-baseline-0e513fe6/step4769
+uv run python -m cookbook.remote gs://ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-actual-5d35521f/step4769  weka://oe-training-default/ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-actual-5d35521f/step4769
+uv run python -m cookbook.remote gs://ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-distill-a2d7cd25/step4769  weka://oe-training-default/ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-distill-a2d7cd25/step4769
+uv run python -m cookbook.remote gs://ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-extract-knowledge-76f1ff02/step4769  weka://oe-training-default/ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-extract-knowledge-76f1ff02/step4769
+uv run python -m cookbook.remote gs://ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-knowledge_list-de0e6931/step4769  weka://oe-training-default/ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-knowledge_list-de0e6931/step4769
+uv run python -m cookbook.remote gs://ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-qa-d2535212/step4769  weka://oe-training-default/ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-qa-d2535212/step4769
+uv run python -m cookbook.remote gs://ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-wrap_medium-ab332b66/step4769  weka://oe-training-default/ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-wrap_medium-ab332b66/step4769
+uv run python -m cookbook.remote gs://ai2-llm/checkpoints/lucas/olmo3-microanneal-olmo3-mix-2b9cd813/step4769  weka://oe-training-default/ai2-llm/checkpoints/lucas/olmo3-microanneal-olmo3-mix-2b9cd813/step4769
+uv run python -m cookbook.remote gs://ai2-llm/checkpoints/lucas/olmo3-microanneal-round1-olmo3_7b-with-reasoning-anneal-03fb7a8d/step4769  weka://oe-training-default/ai2-llm/checkpoints/lucas/olmo3-microanneal-round1-olmo3_7b-with-reasoning-anneal-03fb7a8d/
+uv run python -m cookbook.remote gs://ai2-llm/checkpoints/lucas/olmo3-mix-1b-5xC-b5ad93d5/step4769  weka://oe-training-default/ai2-llm/checkpoints/lucas/olmo3-mix-1b-5xC-b5ad93d5/step4769
+uv run python -m cookbook.remote gs://ai2-llm/checkpoints/mayeec/olmo3-microanneal-round1-olmo3_7b-no-reasoning-anneal-25249a74/step4769 weka://oe-training-default/ai2-llm/checkpoints/mayeec/olmo3-microanneal-round1-olmo3_7b-no-reasoning-anneal-25249a74/step4769
