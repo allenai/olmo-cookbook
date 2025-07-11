d019020ecce6db405108ac6107f756baa560555b
Luca Soldaini <lucas@allenai.org>
2025-07-11 09:21:33 -0700

    new models

--- src/cookbook/recipes/olmo3-midtraining/soldni-web-baseline/launch-evals.sh
+++ src/cookbook/recipes/olmo3-midtraining/soldni-web-baseline/launch-evals.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 
+# models=(
+#     'ai2-llm/checkpoints/lucas/olmo3-microanneal-all-dressed-v18-ce7c0c2a/step4769'
+#     'ai2-llm/checkpoints/lucas/olmo3-microanneal-all-dressed-v20-1e29004d/step4769'
+#     'ai2-llm/checkpoints/lucas/olmo3-microanneal-hq-web-baseline-0e513fe6/step4769'
+#     'ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-actual-5d35521f/step4769'
+#     'ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-distill-a2d7cd25/step4769'
+#     'ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-extract-knowledge-76f1ff02/step4769'
+#     'ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-knowledge_list-de0e6931/step4769'
+#     'ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-qa-d2535212/step4769'
+#     'ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-wrap_medium-ab332b66/step4769'
+#     'ai2-llm/checkpoints/lucas/olmo3-microanneal-olmo3-mix-2b9cd813/step4769'
+#     'ai2-llm/checkpoints/lucas/olmo3-microanneal-round1-olmo3_7b-with-reasoning-anneal-03fb7a8d/step4769'
+#     'ai2-llm/checkpoints/mayeec/olmo3-microanneal-round1-olmo3_7b-no-reasoning-anneal-25249a74/step4769'
+# )
+
 models=(
-    'ai2-llm/checkpoints/lucas/olmo3-microanneal-all-dressed-v18-ce7c0c2a/step4769'
-    'ai2-llm/checkpoints/lucas/olmo3-microanneal-all-dressed-v20-1e29004d/step4769'
-    'ai2-llm/checkpoints/lucas/olmo3-microanneal-hq-web-baseline-0e513fe6/step4769'
-    'ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-actual-5d35521f/step4769'
-    'ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-distill-a2d7cd25/step4769'
-    'ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-extract-knowledge-76f1ff02/step4769'
-    'ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-knowledge_list-de0e6931/step4769'
-    'ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-qa-d2535212/step4769'
-    'ai2-llm/checkpoints/lucas/olmo3-microanneal-nemotron-hq-synth-wrap_medium-ab332b66/step4769'
-    'ai2-llm/checkpoints/lucas/olmo3-microanneal-olmo3-mix-2b9cd813/step4769'
-    'ai2-llm/checkpoints/lucas/olmo3-microanneal-round1-olmo3_7b-with-reasoning-anneal-03fb7a8d/step4769'
-    'ai2-llm/checkpoints/mayeec/olmo3-microanneal-round1-olmo3_7b-no-reasoning-anneal-25249a74/step4769'
+    'ai2-llm/checkpoints/lucas/olmo3-microanneal-with-reasoning-all-dressed-v20-86e7a6de/step4769'
+    'ai2-llm/checkpoints/lucas/olmo3-microanneal-with-reasoning-all-dressed-v18-20-276c09a8/step4769'
+    'ai2-llm/checkpoints/lucas/olmo3-microanneal-with-reasoning-web-nemotron-cc-b3c3561d/step4769'
+    'ai2-llm/checkpoints/lucas/olmo3-microanneal-50web-50synth-qa-92abe47c/step4769'
+    'ai2-llm/checkpoints/lucas/olmo3-microanneal-50web-50synth-wrap-55f2ddc7/step4769'
+    # 'ai2-llm/checkpoints/lucas/olmo3-microanneal-50web-50synth-distill-85ff229d/step4769'
+    # 'ai2-llm/checkpoints/lucas/olmo3-microanneal-50web-50synth-kextract-fb5c70e2/step4769'
+    # 'ai2-llm/checkpoints/lucas/olmo3-microanneal-50web-50synth-klist-841b19ca/step4769'
 )
 
 
