ae2def1a4836b4cf15bedf7c3a156097928001d2
Luca Soldaini <lucas@allenai.org>
2025-07-14 08:56:07 -0700

    tokenization?

--- src/cookbook/recipes/olmo3-midtraining/soldni-web-baseline/launch-evals.sh
+++ src/cookbook/recipes/olmo3-midtraining/soldni-web-baseline/launch-evals.sh
@@ -21,6 +21,10 @@ models=(
     'ai2-llm/checkpoints/lucas/olmo3-microanneal-with-reasoning-web-nemotron-cc-b3c3561d/step4769'
     'ai2-llm/checkpoints/lucas/olmo3-microanneal-50web-50synth-qa-92abe47c/step4769'
     'ai2-llm/checkpoints/lucas/olmo3-microanneal-50web-50synth-wrap-55f2ddc7/step4769'
+    'ai2-llm/checkpoints/lucas/olmo3-nanonanneal-50web-50synthqa-63696db9/step477'
+)
+
+models=(
     'ai2-llm/checkpoints/lucas/olmo3-nanonanneal-10web-90synthqa-06e59598/step477'
     'ai2-llm/checkpoints/lucas/olmo3-nanonanneal-20web-80synthqa-1d1ad922/step477'
     'ai2-llm/checkpoints/lucas/olmo3-nanonanneal-30web-70synthqa-fc122193/step477'
@@ -29,13 +33,10 @@ models=(
     'ai2-llm/checkpoints/lucas/olmo3-nanonanneal-60web-40synthqa-807fdc92/step477'
     'ai2-llm/checkpoints/lucas/olmo3-nanonanneal-70web-30synthqa-e14c2910/step477'
     'ai2-llm/checkpoints/lucas/olmo3-nanonanneal-90web-10synthqa-508971fb/step477'
-)
-
-models=(
     'ai2-llm/checkpoints/lucas/olmo3-nanonanneal-80web-20synthqa-508971fb/step477'
-    # 'ai2-llm/checkpoints/lucas/olmo3-microanneal-50web-50synth-distill-85ff229d/step4769'
-    # 'ai2-llm/checkpoints/lucas/olmo3-microanneal-50web-50synth-kextract-fb5c70e2/step4769'
-    # 'ai2-llm/checkpoints/lucas/olmo3-microanneal-50web-50synth-klist-841b19ca/step4769'
+    'ai2-llm/checkpoints/lucas/olmo3-microanneal-50web-50synth-distill-85ff229d/step4769'
+    'ai2-llm/checkpoints/lucas/olmo3-microanneal-50web-50synth-kextract-fb5c70e2/step4769'
+    'ai2-llm/checkpoints/lucas/olmo3-microanneal-50web-50synth-klist-841b19ca/step4769'
 )
 
 # Moving checkpoints to weka
