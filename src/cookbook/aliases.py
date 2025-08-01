98352fc6d610e7ee64f0ed40650d4738737720bf
Tyler Murray <tylerm@allenai.org>
2025-08-01 12:29:53 -0700

    Must set warmup

--- src/cookbook/aliases.py
+++ src/cookbook/aliases.py
@@ -112,13 +112,17 @@ class SchedulerConfig(BaseModel):
     @field_validator("decay")
     @classmethod
     def validate_decay(cls, v, info):
-        """Validate that decay is mutually exclusive with decay_fraction"""
+        """Validate that decay is mutually exclusive with decay_fraction and not zero"""
+        if v is not None and v == 0:
+            raise ValueError("decay cannot be 0")
         return cls._validate_mutually_exclusive(v, "decay", "decay_fraction", info)
 
     @field_validator("decay_fraction")
     @classmethod
     def validate_decay_fraction(cls, v, info):
-        """Validate that decay_fraction is mutually exclusive with decay"""
+        """Validate that decay_fraction is mutually exclusive with decay and not zero"""
+        if v is not None and v == 0:
+            raise ValueError("decay_fraction cannot be 0")
         return cls._validate_mutually_exclusive(v, "decay_fraction", "decay", info)
 
 
