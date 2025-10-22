c65e55797658493c0199a13a2c6b302322479a6c
IanM <ianmag@cs.washington.edu>
2025-10-22 14:43:53 -0700

    .

--- scripts/run_s3_evals_downstream.sh
+++ scripts/run_s3_evals_downstream.sh
@@ -102,7 +102,7 @@ for parent in "${parents[@]}"; do
     s3_step_path="${s3_parent%/}/${step_name}"
     echo "Evaluating checkpoint: ${s3_step_path}"
     olmo-cookbook-eval evaluate "${s3_step_path}" \
-      --tasks mmlu:bpb \
+      --tasks olmo3:dev:1b:main \
       --priority low \
       --cluster ai2/titan \
       --partition-size 8 \
